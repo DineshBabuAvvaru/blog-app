75e4b1d0f869c3fbda09628ade72095c1f26cd97
DineshBabuAvvaru <102279771+DineshBabuAvvaru@users.noreply.github.com>
2023-09-09 11:19:18 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react"
+import axios from "axios"
+import "bootstrap/dist/css/bootstrap.min.css"
+import {nanoid} from "nanoid"
+import CreatePost from './Components/CreatePost';
+import Posts from './Components/Posts';
 
 function App() {
+  const [data,setData] = useState([])
+  
+  const [show,setShow] = useState(false)
+
+  
+  const API_URL = "https://jsonplaceholder.typicode.com/posts"
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      const response = await axios.get(API_URL)
+      const results = await response.data
+      setData(results)
+    }
+    fetchPosts();
+  },[])
+  const createPost = async (newBody,newTitle,setNewBody,setNewTitle) => {
+    const newPost = {
+      id: data.legth+1,
+      title: newTitle,
+      body: newBody,
+      userId:nanoid()
+    }
+    const response = await axios.post(API_URL,newPost)
+    const results = await response.data
+    console.log(results)
+    setData([...data,results])
+    setShow(false) 
+    setNewBody("")
+    setNewTitle("")
+  }
+  const deletePost = async (id) => {
+    const response = await axios.delete(`${API_URL}/${id}`)
+    const result = await response.data
+    console.log(result)
+    const newData = data.filter((each)=>each.id!==id)
+    setData(newData)
+  }
+  const updatePost = async (id) => {
+    const newPost = {
+      id:id,
+      title:"updated",
+      body:"changed",
+      userId:nanoid()
+    }
+    const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`,newPost)
+    const res = await response.data
+    console.log(res)
+    const newData = data.map((each)=>{
+      return each.id === id ? newPost : each
+    })
+    setData(newData)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <CreatePost show={show} setShow={setShow} createPost={createPost}/>
+    <Posts data={data} updatePost={updatePost} deletePost={deletePost} />
+    </>
   );
 }
 
